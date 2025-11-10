4555dd75e39200a062f4e9255a7727b526963439
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-11-10 13:52:36 +0100

    Add Helix-like line navigation

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -3,5 +3,9 @@ vim.keymap.set("n", "<leader>ch", function()
 	vim.cmd("noh")
 end, { desc = "Clear search highlight" })
 
--- Remap terminal exit
+-- Exit terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+
+-- Helix-like line navigation
+vim.keymap.set("n", "gl", "$", { noremap = true })
+vim.keymap.set("n", "gh", "0", { noremap = true })
