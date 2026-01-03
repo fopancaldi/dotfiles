a0419dde9a5342b43000eb90dff7a3c62b7d3743
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-03 14:30:11 +0100

    Start cleaning up setup script

--- not_stowed/setup.bash
+++ not_stowed/setup.bash
@@ -1,25 +1,14 @@
 #!/bin/bash
 
+INITIAL_DIR="$(pwd | tr -d '\r')"
+cd "${HOME}/dotfiles/not_stowed" || exit $?
+
+scripts/make_dirs.sh
+stow ..
+
+cd "${INITIAL_DIR}" || exit $?
+
 scripts/install_agave.sh
 scripts/add_aliases.sh
 
 exit
-
-# TODO: Remove this stuff
-
-echo "echoed from setup.sh"
-
-last_brc_line=$(tail -1 "${HOME}"/.bashrc)
-echo "last line: ${last_brc_line}"
-
-a=$(grep "source \"\${HOME}/dotfiles/NotStowed/aliases.sh\"" ~/.bashrc)
-echo "${a}"
-
-if [ "${a}" != "" ]; then
-echo "should not do anything"
-fi
-
-var=""
-if [ -z "${var}" ]; then
-echo "shirou"
-fi
