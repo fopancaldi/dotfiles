5a80fbb8b5c49fafbdefcb04a9af35507a75a400
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-03 18:25:58 +0100

    Finish superuser setup script

---
+++ scripts/setup_su.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+scripts/common/assert_on_linux.bash
+
+initial_dir="$(pwd | tr -d '\r')"
+cd "${HOME}/dotfiles" || exit $?
+
+for file in "greetd/config.toml" "keyd/default.conf"; do
+  target="/etc/$file"
+  file="${HOME}/dotfiles/${file}"
+
+  echo "Backing up $target"
+  sudo -k scripts/common/smart_mv_bak.bash "$target"
+
+  echo "Linking $file to $target"
+  sudo -k ln -s "$file" "$target"
+done
+unset file
+
+cd "${initial_dir}" || exit $?
+exit
