369b9883d8314f7c08d2f5af8e0d0e2a7594c3f1
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-04 15:12:16 +0100

    Use stow in setup_su too

--- scripts/setup_su.bash
+++ scripts/setup_su.bash
@@ -2,20 +2,26 @@
 
 scripts/common/assert_on_linux.bash
 
+shopt -s globstar
+
 initial_dir="$(pwd | tr -d '\r')"
 cd "${HOME}/dotfiles" || exit $?
 
-for file in "greetd/config.toml" "keyd/default.conf"; do
-  target="/etc/${file}"
-  file="${HOME}/dotfiles/${file}"
+for file in etc/**; do
+  [[ ! -f "$file" ]] && continue
+  file="/${file}"
+  dir="$(dirname "$file")/"
 
-  echo "Backing up $target"
-  sudo -k scripts/common/smart_mv_bak.bash "$target"
+  echo "Creating $dir"
+  sudo -k mkdir -p "$dir"
 
-  echo "Linking $file to $target"
-  sudo -k ln -s "$file" "$target"
+  echo "Backing up $file"
+  sudo -k scripts/common/smart_mv_bak.bash "$file"
 done
-unset file
+unset -v file dir
+
+echo "Stowing etc"
+sudo -k stow --target /etc etc
 
 cd "$initial_dir" || exit $?
 exit
