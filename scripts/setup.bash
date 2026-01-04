0d9428e9e5637b5b1a2359771ba7802e7a96dd23
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-04 10:20:02 +0100

    Rename stow to tilde

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -6,12 +6,12 @@ initial_dir="$(pwd | tr -d '\r')"
 cd "${HOME}/dotfiles" || exit $?
 
 for file in ".bashrc" ".bash_profile" ".bash_logout"; do
- scripts/common/smart_mv_bak.bash "${HOME}/${file}"
+    scripts/common/smart_mv_bak.bash "${HOME}/${file}"
 done
 unset file
 
 scripts/nosu/make_dirs.bash
-stow stow
+stow --target "$HOME" tilde
 
 cd "$initial_dir" || exit $?
 exit
