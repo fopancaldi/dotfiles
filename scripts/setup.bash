0896c4db5fcb869ff85d4bfb813b14db91d63891
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-10 15:20:08 +0100

    Fix created directory

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -9,7 +9,7 @@ for file in ".bashrc" ".bash_profile" ".bash_logout" ".bash_aliases"; do
     scripts/common/smart_mv_bak.bash "${HOME}/${file}"
 done
 unset -v file
-mkdir -p "${HOME}/Pictures/Screenshots"
+mkdir -p "${HOME}/Pictures/Wallpapers"
 
 stow --target "$HOME" tilde
 
