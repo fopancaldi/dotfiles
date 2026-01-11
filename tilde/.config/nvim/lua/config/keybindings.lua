75b76590c6e0f3cfd781df23119388fc9b176f2b
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-11 19:32:04 +0100

    Improve keybinding description

--- tilde/.config/nvim/lua/config/keybindings.lua
+++ tilde/.config/nvim/lua/config/keybindings.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>ch", function()
   vim.api.nvim_command("noh")
 end, { desc = "Clear search highlight", noremap = true })
 
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal", noremap = true })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal insert mode", noremap = true })
 
 vim.keymap.set("n", "gl", "$", { desc = "Go to line begin", noremap = true })
 vim.keymap.set("n", "gh", "0", { desc = "Go to line end", noremap = true })
