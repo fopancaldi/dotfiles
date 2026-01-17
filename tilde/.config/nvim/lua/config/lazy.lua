97b5effb686c0af7c370c42d18b06fb708ddc42b
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-17 10:27:39 +0100

    Move lazy-lock.json out of the way

--- tilde/.config/nvim/lua/config/lazy.lua
+++ tilde/.config/nvim/lua/config/lazy.lua
@@ -16,4 +16,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    import = "plugins",
+  },
+  lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json",
+  install = {
+    colorscheme = {
+      "retrobox",
+    },
+  },
+})
