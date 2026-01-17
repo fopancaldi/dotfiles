c1deaf740d5dc917be2ead225c7879e771c87d7c
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-17 16:48:34 +0100

    Return to non-opts config

--- tilde/.config/nvim/lua/plugins/treesitter.lua
+++ tilde/.config/nvim/lua/plugins/treesitter.lua
@@ -2,25 +2,28 @@ return {
   "nvim-treesitter/nvim-treesitter",
   version = "0.10.0",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "bibtex",
-      "cpp",
-      "cuda",
-      "fortran",
-      --"latex",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "regex",
-      "vim",
-      "vimdoc",
-      "xml",
-    },
-    highlight = {
-      enable = true,
-    },
-    indent = { enable = true },
-  },
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "bash",
+        "bibtex",
+        "cpp",
+        "cuda",
+        "fortran",
+        --"latex",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "vim",
+        "vimdoc",
+        "xml",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+    })
+  end,
 }
