838d208c2982a75992d2f4357231f7afe3d1482e
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-01-26 18:43:48 +0100

    Add swayimg autocmd

--- tilde/.config/nvim/lua/config/autocmd.lua
+++ tilde/.config/nvim/lua/config/autocmd.lua
@@ -5,3 +5,7 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { "*.pdf" },
   command = "execute \"!zathura '%'\" | bdelete %",
 })
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.png" },
+  command = "execute \"!swayimg '%'\" | bdelete %",
+})
