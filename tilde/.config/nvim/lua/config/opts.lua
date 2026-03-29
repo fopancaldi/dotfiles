dd1c019536200e1e6b3a033a70a0836d0cbbd4b4
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-03-29 20:51:37 +0200

    Change weird default selection behaviour

--- tilde/.config/nvim/lua/config/opts.lua
+++ tilde/.config/nvim/lua/config/opts.lua
@@ -5,6 +5,7 @@ vim.opt.guicursor = "n-v-ve:block,i-r-c-ci-cr-t:ver25,o:hor50,a:blinkon0"
 vim.opt.ignorecase = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.selection = "inclusive"
 vim.opt.scrolloff = 10
 
 vim.opt.softtabstop = 4
