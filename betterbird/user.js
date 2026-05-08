ea5307a9e03330207f1f16106c8f3f690b84ff92
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-05-08 15:22:04 +0200

    Reorder and default mail to plain text

--- betterbird/user.js
+++ betterbird/user.js
@@ -1,7 +1,8 @@
-user_pref("mail.pane_config.dynamic", 2);
-user_pref("mail.shell.checkDefaultClient", false);
+user_pref("mail.biff.show_tray_icon_always", true);
+user_pref("mail.default_send_format", 5);
 user_pref("mail.minimizeToTray", true);
 user_pref("mail.minimizeToTray.supportedDesktops", "kde,gnome,pop:gnome,xfce,mate,sway");
-user_pref("mail.biff.show_tray_icon_always", true);
-user_pref("mailnews.start_page.enabled", false);
+user_pref("mail.pane_config.dynamic", 2);
+user_pref("mail.shell.checkDefaultClient", false);
 user_pref("mail.startupMinimized", true);
+user_pref("mailnews.start_page.enabled", false);
