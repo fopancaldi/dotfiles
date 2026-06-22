e471ff7b24986fb4379c8f7c4cf29d6db8bd93e3
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-06-22 18:34:38 +0200

    Add haskell lsp config

--- tilde/.config/nvim/lua/config/lsp.lua
+++ tilde/.config/nvim/lua/config/lsp.lua
@@ -2,5 +2,8 @@ vim.lsp.config("*", {
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
+vim.lsp.config("hls", {})
+vim.lsp.enable("hls")
+
 vim.lsp.config("rust_analyzer", {})
 vim.lsp.enable("rust_analyzer")
